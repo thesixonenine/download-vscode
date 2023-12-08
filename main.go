9949236ec6bb3fa84cfd0426b65f627c7b461a61
Simple <huyang1994822@gmail.com>
2023-12-08 11:07:22 +0800

    download

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"path"
+	"strings"
 )
 
 // main download vscode use cdn at cn
@@ -19,8 +24,62 @@ func main() {
 	}
 	resp, _ := client.Get("https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive")
 	location, _ := resp.Location()
-	a := location.Scheme + "://" + location.Host + location.Path
-	fmt.Println(a)
-	a = location.Scheme + "://vscode.cdn.azure.cn" + location.Path
-	fmt.Println(a)
+	host := location.Host
+	queryPath := location.Path
+	originUrl := location.Scheme + "://" + host + queryPath
+	cdnUrl := location.Scheme + "://vscode.cdn.azure.cn" + queryPath
+	split := strings.Split(queryPath, "/")
+	fileName := split[len(split)-1]
+	ext := path.Ext(fileName)
+	fileN := strings.TrimSuffix(fileName, ext)
+	fmt.Printf("最新文件名:\n%s|%s\n", fileN, fileName)
+	fmt.Printf("原始下载链接:\n%s\n", originUrl)
+	fmt.Printf("CDN下载链接:\n%s\n", cdnUrl)
+	// 开始下载文件
+	resp, err := http.Get(cdnUrl)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	if resp.StatusCode == 404 {
+		fmt.Printf("CDN还未缓存文件[%s],是否继续使用原始链接下载?[y/n] ", fileName)
+		flag := ""
+		_, err := fmt.Scanln(&flag)
+		if err != nil {
+			log.Fatalf("输入错误: %s", err.Error())
+		}
+		flag = strings.TrimSpace(flag)
+		if strings.EqualFold(flag, "y") {
+			resp, err = http.Get(originUrl)
+		} else {
+			return
+		}
+	}
+	if resp.StatusCode != 200 {
+		log.Fatalf("文件[%s]下载失败,code[%d],msg[%s]", fileName, resp.StatusCode, resp.Status)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Fatalf(err.Error())
+		}
+	}()
+	out, err := os.Create(fileName)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	defer func() {
+		if err := out.Close(); err != nil {
+			log.Fatalf(err.Error())
+		}
+	}()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func ZIPTo7z(fn string) {
+	format := "mkdir tmp && Copy-Item FILE_NAME.zip tmp && cd tmp && 7z x FILE_NAME.zip && Remove-Item FILE_NAME.zip && 7z a -t7z FILE_NAME.7z * && Copy-Item FILE_NAME.7z ../ && cd .. && Remove-Item -Force -Recurse tmp"
+	command := strings.ReplaceAll(format, "FILE_NAME", fn)
+	exec.Command(command)
 }
